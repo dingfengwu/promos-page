de990da969da54dc7532471a6ed104155ae6bb81
dingfengwu <dingfeng_wu@hotmail.com>
2022-08-10 19:36:13 +0800

    保存

---
+++ src/components/promos.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="promos" @click="openDownloadLink">
+
+  </div>
+</template>
+
+<script>
+export default {
+  methods:{
+    openDownloadLink(){
+      
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.promos{
+  background-image: url("../assets/body-bg.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100vh;
+  overflow: hidden;
+}
+</style>
